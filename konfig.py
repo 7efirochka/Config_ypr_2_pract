4b294ec21f94994d6b639f64a546c27064e0160c
Galina <s7ombra@gmail.com>
2025-10-20 12:04:31 +0300

    branch up

--- konfig.py
+++ konfig.py
@@ -8,3 +8,19 @@ print(f"License: {getattr(mpl, '__license__', 'Not specified')}")
 print(f"Config directory: {mpl.get_configdir()}")
 print(f"Cache directory: {mpl.get_cachedir()}")
 
+
+dot_code = """
+digraph MatplotlibDeps {
+    rankdir=LR;
+    node [shape=box];
+
+    matplotlib -> numpy;
+    matplotlib -> pillow;
+    matplotlib -> cycler;
+    matplotlib -> fonttools;
+    matplotlib -> kiwisolver;
+}
+"""
+
+with open('../matplotlib_simple.dot', 'w') as f:
+    f.write(dot_code)
